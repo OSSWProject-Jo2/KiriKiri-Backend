a0827d76b227c1007af0bdc347d3fd495dd59be1
uols2170065 <yebeom0718@gmail.com>
2026-05-02 19:16:03 +0900

    Post 엔티티 DataInitializer에 추가 후 작동 여부 확인함

--- src/main/java/com/example/party_finder/DataInitializer.java
+++ src/main/java/com/example/party_finder/DataInitializer.java
@@ -1,7 +1,10 @@
 package com.example.party_finder;
 
+import com.example.party_finder.domain.Post;
+import com.example.party_finder.domain.PostRepository;
 import com.example.party_finder.domain.User;
 import com.example.party_finder.domain.UserRepository;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -9,24 +12,40 @@ import org.springframework.stereotype.Component;
 public class DataInitializer implements CommandLineRunner { // 2. 서버 켜지자마자 실행하라는 약속
 
     private final UserRepository userRepository;
+    private final PostRepository postRepository;
 
-    // 3. 생성자를 통해 User의 Repository를 데려온다.
-    public DataInitializer(UserRepository userRepository) {
+    // 3. 생성자를 통해 User의 Repository, Post의 Repository를 데려온다.
+    public DataInitializer(UserRepository userRepository, PostRepository postRepository) {
         this.userRepository = userRepository;
+        this.postRepository = postRepository;
     }
-
     @Override
     public void run(String... args) {
         // 4. 저장할 유저 정보를 담은 객체를 만든다.
-        User user = new User();
-        user.setUsername("test_user");
-        user.setPassword("1234");
-        user.setNickname("test_nickname");
-
-        // 5. 비서에게 이 유저를 DB에 저장하라고 시킨다.
         if (userRepository.findByUsername("test_user").isEmpty()) {
+            User user = new User();
+            user.setUsername("test_user");
+            user.setPassword("1234");
+            user.setNickname("초보개발자");
             userRepository.save(user);
+            System.out.println("유저 데이터 DB입력 작동 여부 확인");
         }
-        System.out.println("유저 데이터 DB입력 작동 여부 확인");
+
+
+        if (postRepository.count() == 0) {
+            Post samplePost = new Post();
+            samplePost.setTitle("첫 번째 게시글"); // 제목 설정
+            samplePost.setContent("비회원 게시판 테스트 중입니다."); // 본문 설정
+            samplePost.setAuthor("ㅇㅇ"); // 비회원 이름
+            samplePost.setPassword("5678"); // 나중에 삭제할 때 쓸 비밀번호
+
+            // 4. 비서에게 저장 명령 (이때 SQL의 INSERT 문이 실행됩니다)
+            postRepository.save(samplePost);
+
+            System.out.println("게시글 데이터 DB입력 작동 여부 확인");
+        }
+
+
+
     }
 }
